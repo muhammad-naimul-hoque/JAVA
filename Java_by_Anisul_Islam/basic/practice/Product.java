371f1257d69eb3581af57cac53544eb7fbc7a244
Muhammad Naimul Hoque <naimulhoq89@gmail.com>
2024-08-15 21:26:15 +0600

    first program, datatype, comment

---
+++ Java_by_Anisul_Islam/basic/practice/Product.java
@@ -0,0 +1,30 @@
+public class Product {
+    public static void main(String[] args) {
+        int id;
+        String title;
+        String price;
+        String description;
+        String category;
+
+        id = 101;
+        title = "iphone15";
+        price = "1895 euros";
+        description = "perfect product with best image quality.";
+        category = "phone";
+
+        System.out.println("id : " + id);
+        System.out.println("title : " + title);
+        System.out.println("price : " + price);
+        System.out.println("description : " + description);
+        System.out.println("category : " + category);
+    }
+}
+/*
+ * Assignment-2 (Variable and Data type)
+ * step 1: create a class called Product
+ * step 2: create a main method
+ * step 3: declare variables: id, title, price, description, category
+ * step 4: assign the following data in main method
+ * 101,iphone15,1895 euros,perfect product with best image quality, phone,
+ * step 5: print the data
+ */
